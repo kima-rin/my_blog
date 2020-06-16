0b0d034db23b03a3d9adf4207efa295f4bf06b5c
kima-rin <https://github.com/kima-rin/my_blog.git>
2020-06-16 14:59:10 +0900

    2차메뉴

--- part/head.php
+++ part/head.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>블로그</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="/resource/common.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/resource/common.js"></script>
 </head>
 
 <body>
-<div class="top-bar">
+<div>
         <div class="mobile-top-bar visible-sm-down flex">
             <a href="#" class="btn-toggle-mobile-side-bar flex-as-c">
                 <div></div>
@@ -19,8 +21,13 @@
             </a>
         </div>
     </div>
-<div class="top-bar">
+<div class="mobile-side-bar-bg">
+    </div>
+    <div class="mobile-side-bar">
+</div>    
+<div class="top-bar visible-md-up">
         <div class="con height-100p flex">
+        <a href="/" class="logo flex flex-ai-c"><i class="fab fa-artstation"></i></a>
             <nav class="menu-box-1 flex flex-1-0-0">
                 <ul class="flex flex-1-0-0">
                     <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
@@ -39,3 +46,19 @@
             </nav>
         </div>
     </div>
+<div class="mobile-side-bar">
+  <nav class="mobile-menu-bar">
+      <ul>
+          <li><a href="#" class="block">Home</a></li>
+          <li><a href="#" class="block">Articles</a></li>
+          <li><a href="#" class="block">About Me</a></li>
+          <li><a href="#" class="block">SNS</a>
+          <ul>
+              <li><a href="#" class="block">GITHUD</a></li>
+              <li><a href="#" class="block">FACE BOOK</a></li>
+              <li><a href="#" class="block">INSTAGRAM</a></li>
+          </ul>
+          </li>
+      </ul>
+  </nav>
+</div>
