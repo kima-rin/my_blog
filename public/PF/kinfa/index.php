f2effa259a55fa5e13eb6e3d072ffb2d3fd430bc
kima-rin <https://github.com/kima-rin/my_blog.git>
2020-07-10 12:19:43 +0900

    모작 수정중

--- public/PF/kinfa/index.php
+++ public/PF/kinfa/index.php
@@ -44,10 +44,13 @@
         <div class="menu-box con">
             <nav class="">
                 <ul class="flex flex-ai-c flex-jc-c">
-                    <li class="flex-1-0-0"><a href="#" cless="block border-red">휴먼예금 관리</a>
+                    <li class="flex-1-0-0"><a href="#" cless="block border-red">휴면예금 관리</a>
                         <div>
                             <div>
-                                <ul>
+                                <a>휴면예금 관리</a>
+                            </div>
+                            <div class="con">
+                                <ul class="flex">
                                     <li><a href="#">휴면예금 조회 및<br>지급신청따라하기</a></li>
                                     <li><a href="#">휴면예금 조회 및<br>지급신청</a></li>
                                     <li><a href="#">출연휴면예금 상속인<br>조회신청 따라하기</a></li>
@@ -59,7 +62,7 @@
                     <li class="flex-1-0-0"><a href="#" cless="block">일반기부</a>
                         <div>
                             <div>
-                                <ul>
+                                <ul class="flex">
                                     <li><a href="#">기부 따라하기</a></li>
                                     <li><a href="#">일반 기부자</a></li>
                                     <li><a href="#">기부자 안내</a></li>
@@ -72,7 +75,7 @@
                     <li class="flex-1-0-0"><a href="#" cless="block">서비스안내</a>
                         <div>
                             <div>
-                                <ul>
+                                <ul class="flex">
                                     <li><a href="#">휴면예금 안내</a></li>
                                     <li><a href="#">기부하기 안내</a></li>
                                     <li><a href="#">출연 휴면예금 상속인 조회</a></li>
@@ -85,7 +88,7 @@
                     <li class="flex-1-0-0"><a href="#" cless="block">고객지원</a>
                         <div>
                             <div>
-                                <ul>
+                                <ul class="flex">
                                     <li><a href="#">공지사항</a></li>
                                     <li><a href="#">자주하는 질문</a></li>
                                     <li><a href="#">프로그램 설치</a></li>
@@ -100,7 +103,7 @@
                     <li class="flex-1-0-0"><a href="#" cless="block">나의 정보 조회</a>
                         <div>
                             <div>
-                                <ul>
+                                <ul class="flex">
                                     <li><a href="#">신청 내역 조회 및<br>증명서 발급</a></li>
                                 </ul>
                             </div>
