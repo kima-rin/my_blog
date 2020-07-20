d312586938c2dcf58bbf96de538288dc78a02f89
kima-rin <anmoonlee@gmail.com>
2020-07-20 18:15:49 +0900

    eight

---
+++ public/PF/eig/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>eightthethalasso.jp</title>
+    <!--폰트어썸 불러오기-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="/PF/eig/index.css">
+    <!--font-->
+    <style>
+        @font-face {
+            font-family: 'A1明朝';
+            font-weight: Bold;
+            src: url(//wf.typesquare.com/3/tsst/dist/ja/ts?condition=d244f2919b886393d86b418e5a67627f&eid=DYi2pw~QmcU%3D&bw[name]=Chrome&bw[ftf]=0&bw[os]=Windows&location=eightthethalasso.jp%2F&fonts[id]=2146&fonts[str]=MOOAgeOAguOBhOOBjeOBpOOBp%2BOBq%2BOBruOBt%2BOBuOOCiOOCi%2BOCkuOCk%2BOCouOCreOCseOCteOCuOOCueOCveOCv%2BODgOODieODkeODluODmOODoeODqeODs%2BODvOS%2FruWIhuW5ueW6puaDs%2BaIkOaltea1uOa9pOa%2Fg%2BeZuueptue0sOiDnuijnOmAj%2BmrmOmrqg%3D%3D);
+        }
+    </style>
+    <!--제이쿼리 불러오기-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- owl 캐러셀 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
+    <script src="/PF/Bon/index.js"></script>
+</head>
+<body>
+<section class="first-view">
+    <div class="title-box">
+        <img src="https://kima-rin.github.io/img1/eig/6e7e646.svg" alt="">
+    </div>
+    <div class="main-img"></div>
+    <div class="text-box-1">
+        <div>タラソ幹細胞成分で、ぷるん髪。<br>スキンケア発想のヘアケアブランド。</div>
+        <div></div>
+        <div></div>
+    </div>
+
+</section> 
+
+</body>
+</html>
