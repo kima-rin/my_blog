c4b1bba12f3340a7c6dee7f79acd74cf672ad94c
kima-rin <https://github.com/kima-rin/my_blog.git>
2020-07-21 02:58:10 +0900

    3차 모작 시작

--- public/PF/eig/index.php
+++ public/PF/eig/index.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="/PF/eig/index.css">
     <!--font-->
+    <script
+        src="https://googleads.g.doubleclick.net/pagead/viewthroughconversion/707567814/?random=1595256577847&amp;cv=9&amp;fst=1595256577847&amp;num=1&amp;label=RiVaCPiFvLcBEMbBstEC&amp;guid=ON&amp;resp=GooglemKTybQhCsO&amp;u_h=1080&amp;u_w=1920&amp;u_ah=1040&amp;u_aw=1920&amp;u_cd=24&amp;u_his=2&amp;u_tz=540&amp;u_java=false&amp;u_nplug=3&amp;u_nmime=4&amp;gtm=2wg783&amp;sendb=1&amp;ig=1&amp;frm=0&amp;url=http%3A%2F%2Feightthethalasso.jp%2F&amp;tiba=%E3%80%90%E5%85%AC%E5%BC%8F%E3%80%918%20THE%20THALASSO%EF%BC%88%E3%82%A8%E3%82%A4%E3%83%88%E3%82%B6%E3%82%BF%E3%83%A9%E3%82%BD%EF%BC%89%EF%BD%9C%20%E3%82%BF%E3%83%A9%E3%82%BD%E5%B9%B9%E7%B4%B0%E8%83%9E%E6%88%90%E5%88%86%E3%81%A7%E3%80%81%E3%81%B7%E3%82%8B&amp;hn=www.googleadservices.com&amp;async=1&amp;rfmt=3&amp;fmt=4">
+    </script>
     <style>
         @font-face {
             font-family: 'A1明朝';
@@ -17,6 +20,7 @@
             src: url(//wf.typesquare.com/3/tsst/dist/ja/ts?condition=d244f2919b886393d86b418e5a67627f&eid=DYi2pw~QmcU%3D&bw[name]=Chrome&bw[ftf]=0&bw[os]=Windows&location=eightthethalasso.jp%2F&fonts[id]=2146&fonts[str]=MOOAgeOAguOBhOOBjeOBpOOBp%2BOBq%2BOBruOBt%2BOBuOOCiOOCi%2BOCkuOCk%2BOCouOCreOCseOCteOCuOOCueOCveOCv%2BODgOODieODkeODluODmOODoeODqeODs%2BODvOS%2FruWIhuW5ueW6puaDs%2BaIkOaltea1uOa9pOa%2Fg%2BeZuueptue0sOiDnuijnOmAj%2BmrmOmrqg%3D%3D);
         }
     </style>
+    <link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@700&display=swap" rel="stylesheet">
     <!--제이쿼리 불러오기-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
@@ -25,24 +29,51 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
-    <script src="/PF/Bon/index.js"></script>
-</head>
-<body>
-<section class="first-view">
-    <div class="title-box">
-        <img src="https://kima-rin.github.io/img1/eig/6e7e646.svg" alt="">
-    </div>
-    <div class="main-img"></div>
-    <div class="text-box-1">
-        <div>about</div>
-        <div>
-            <div>タラソ幹細胞成分で、ぷるん髪。<br>スキンケア発想のヘアケアブランド。</div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
+    <script src="/PF/eig/index.js"></script>
 
-</section> 
+</head>
+
+<body>
+    <section class="first-view">
+        <div class="title-box">
+            <img src="https://kima-rin.github.io/img1/eig/6e7e646.svg" alt="">
+        </div>
+        <div class="main-img"></div>
+
+    </section>
+    <section>
+        <div class="text-box-1 flex">
+            <div>
+                <p>ABOUT</p>
+            </div>
+            <div>
+                <div class="text-style-1">タラソ幹細胞成分で、ぷるん髪。<br>スキンケア発想のヘアケアブランド。</div>
+                <div class="text-style-2"></div>
+                <div class="text-style-3">それは、“洗う保水美容液”。<br>最先端のスキンケア技術をかけ合わせて生まれた<br>新発想のヘアケアブランドです。</div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="second-img-box">
+            <div class="img-box-1"></div>
+        </div>
+        
+    </section>
+    <section>
+        <div class="third-img-box border-red">
+            <div class="relative">
+                <div class="text-style-1">潤いの、究極へ。<br>ダメージによるパサつきを、浸透補修。</div>
+                <div class="text-style-2"></div>
+                <div class="text-style-3">幹細胞や海洋由来の美容成分をブレンドした
+                    <br>「タラソ幹細胞成分」を配合。<br>ダメージによるパサつきやうねりに悩む髪を浸透補修し、<br>内側からぷるんと潤う、しっとりとした艶髪へ。</div>
+            </div>
+            <div class="img-box-1">
+                <div></div>
+            </div>
+        </div>
+
+    </section>
 
 </body>
+
 </html>
