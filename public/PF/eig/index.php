821a508f220d0277945013d681c9eed979af0e7e
kima-rin <anmoonlee@gmail.com>
2020-07-23 18:44:16 +0900

    3차 모작 레이아웃완성

--- public/PF/eig/index.php
+++ public/PF/eig/index.php
@@ -47,9 +47,38 @@
         </div>
 
     </section>
+    <div class="news-1 flex con">
+        <div class="flex-1-0-0 news-title">NEWS</div>
+        <div class="text-deco-underline flex flex-jc-end flex-ai-c">ALL NEWS</div>
+
+    </div>
+    <div class="news-2 con">
+        <div>
+            <div
+                style="background-image:url(http://images.ctfassets.net/cvw67xv1x77g/1Q4137g7gK1EcMMPaoM4Nf/6e3cd1e81dd86b12166f89a314220acb/KV.jpg)">
+            </div>
+            <p>
+                髪のダメージによる空洞化をケアしながら、毛先までさらさらの指通り“するん髪”が叶う！スムースリペアシリーズが新登場
+                </p>
+        </div>
+        <div>
+            <div
+                style="background-image:url(http://images.ctfassets.net/cvw67xv1x77g/6skuLnFrvZBNjajfpejsWG/02205a8ef276b5e0b802e87058631fb2/firstview_picture.jpg)">
+            </div>
+            <p>
+            新発売の美容液プレシャンプーのミニサイズ付き!【夏季限定】爽快感のある仕上がりのスパ限定キット登場
+                  
+            </p>
+        </div>
+
+        <div>
+            
+        </div>
+
+    </div>
     <section>
         <div class="text-box-1 flex">
-            <div>
+            <div class="about">
                 <p>ABOUT</p>
             </div>
             <div>
@@ -63,10 +92,10 @@
         <div class="second-img-box">
             <div class="img-box-1"></div>
         </div>
-        
+
     </section>
     <section>
-        <div class="third-img-box border-red">
+        <div class="third-img-box">
             <div class="relative">
                 <div class="text-style-1">潤いの、究極へ。<br>ダメージによるパサつきを、浸透補修。</div>
                 <div class="text-style-2"></div>
@@ -82,59 +111,126 @@
         <p>IN BATH</p>
     </div>
     <div class="container">
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/1cf4d39.png" alt="">
-        <div class="prod-list-title">PRE-SHAMPOO</div>
-        <div class="prod-list-sud-title">RESET CLEANSING & HEADSPA </div>
-        <div class="prod-list-intro"><br>エイトザタラソ <br>リセットクレンジング＆ヘッドスパ<br>美容液プレシャンプー <br>200mL/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/1cf4d39.png" alt="">
+            <div class="prod-list-title">PRE-SHAMPOO</div>
+            <div class="prod-list-sud-title">RESET CLEANSING & HEADSPA </div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br>リセットクレンジング＆ヘッドスパ<br>美容液プレシャンプー <br>200mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/73bcbcc.jpg" alt="">
+            <div class="prod-list-title">SHAMPOO</div>
+            <div class="prod-list-sud-title">CLEANSING REPAIR & MOIST</div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br> クレンジングリペア&モイスト<br>美容液シャンプー<br>475mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/5015d22.jpg" alt="">
+            <div class="prod-list-title">TREATMENT</div>
+            <div class="prod-list-sud-title">DEEP REPAIR & AQUA MOIST </div>
+            <div class="prod-list-intro"> <br> エイトザタラソ <br> ディープリペア＆アクアモイスト<br> 美容液トリートメント<br>475mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/25ccf90.jpg" alt="">
+            <div class="prod-list-title">HAIR MASK</div>
+            <div class="prod-list-sud-title">RICH COAT & HIGH MOIST </div>
+            <div class="prod-list-intro"><br><br> エイトザタラソ <br> リッチコート＆ハイモイスト<br> 美容液ヘアマスク<br>200mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/f33c68b.png" alt="">
+            <div class="prod-list-title">SHAMPOO</div>
+            <div class="prod-list-sud-title">CLEANSING REPAIR & SMOOTH</div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br> クレンジングリペア＆スムース<br> 美容液シャンプー<br>475mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/b41b477.png" alt="">
+            <div class="prod-list-title">TREATMENT</div>
+            <div class="prod-list-sud-title">SMOOTH REPAIR & AQUA SERUM</div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br> スムースリペア＆アクアセラムト<br> 美容液トリートメント<br>475mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/184f733.png" alt="">
+            <div class="prod-list-title">HAIR MASK</div>
+            <div class="prod-list-sud-title">SMOOTH COAT & RICH SERUM</div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br> スムースコート＆リッチセラム<br> 美容液ヘアマスク<br>200g/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="line-up">LINE UP</div>
+
     </div>
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/73bcbcc.jpg" alt="">
-        <div class="prod-list-title">SHAMPOO</div>
-        <div class="prod-list-sud-title">CLEANSING REPAIR & MOIST</div>
-        <div class="prod-list-intro"><br>エイトザタラソ <br> クレンジングリペア&モイスト<br>美容液シャンプー<br>475mL/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
+    <div class="outBath">
+        <p>OUT BATH</p>
     </div>
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/5015d22.jpg" alt="">
-        <div class="prod-list-title">TREATMENT</div>
-        <div class="prod-list-sud-title">DEEP REPAIR & AQUA MOIST </div>
-        <div class="prod-list-intro"> <br> エイトザタラソ <br> ディープリペア＆アクアモイスト<br> 美容液トリートメント<br>475mL/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
+
+    <div class="container-2">
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/891b7ed.jpg" alt="">
+            <div class="prod-list-title">HAIR OIL</div>
+            <div class="prod-list-sud-title">REPAIR SHOT & EX MOIST </div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br>リペアショット&EXモイスト<br>美容液オイル<br>100mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+        <div class="item">
+            <img src="http://eightthethalasso.jp/_nuxt/img/7c16b1d.png" alt="">
+            <div class="prod-list-title">HAIR OIL</div>
+            <div class="prod-list-sud-title">BALANCING SERUM & SMOOTH REPAIR</div>
+            <div class="prod-list-intro"><br>エイトザタラソ <br> バランシングセラム＆スムースリペア<br>美容液オイル<br>100mL/¥1400(税抜)</div>
+            <div><a href="#" class="prod-list-more block">More</a></div>
+        </div>
+
     </div>
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/25ccf90.jpg" alt="">
-        <div class="prod-list-title">HAIR MASK</div>
-        <div class="prod-list-sud-title">RICH COAT & HIGH MOIST </div>
-        <div class="prod-list-intro"> <br> エイトザタラソ <br> リッチコート＆ハイモイスト<br> 美容液ヘアマスク<br>200mL/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
-       
+    <div class="container-3">
+        <div class="item">
+            <img src="https://scontent-dfw5-2.cdninstagram.com/v/t51.29350-15/109490007_987253298459719_7916020440195629050_n.jpg?_nc_cat=104&_nc_sid=8ae9d6&_nc_ohc=3FV1bMrE5c4AX-25Qex&_nc_ht=scontent-dfw5-2.cdninstagram.com&oh=c7dec56766fbff8498a5ec82177b484a&oe=5F408BC0"
+                alt="">
+        </div>
+        <div class="item">
+            <img src="https://scontent-dfw5-2.cdninstagram.com/v/t51.29350-15/109272118_275344273721006_2837267455506977427_n.jpg?_nc_cat=108&_nc_sid=8ae9d6&_nc_ohc=fFxV1DAkHQ0AX_H2Sp0&_nc_ht=scontent-dfw5-2.cdninstagram.com&oh=d3207a1a05ffaca0e2ea27df28b9da4c&oe=5F3ED32B"
+                alt=""></div>
+        <div class="item">
+            <img src="https://scontent-dfw5-2.cdninstagram.com/v/t51.29350-15/107901056_1662253120591853_5654346366048971325_n.jpg?_nc_cat=104&_nc_sid=8ae9d6&_nc_ohc=B3tTA6hkhg4AX94kTuB&_nc_ht=scontent-dfw5-2.cdninstagram.com&oh=fa5f29fd0a8b0bb6e2e3d8470d3f73c2&oe=5F3D1ED1"
+                alt=""></div>
+        <div class="item">
+            <img src="https://scontent-dfw5-2.cdninstagram.com/v/t51.29350-15/107273331_1618925391603739_5458839204151753253_n.jpg?_nc_cat=102&_nc_sid=8ae9d6&_nc_ohc=ksnrl6ACoWQAX--OBL9&_nc_ht=scontent-dfw5-2.cdninstagram.com&oh=750084384d378affe06915d907f78dd8&oe=5F3F5C17"
+                alt=""></div>
+        <div class="item">
+            <img src="https://scontent-dfw5-2.cdninstagram.com/v/t51.29350-15/107321302_566049847394948_7024172925973581242_n.jpg?_nc_cat=100&_nc_sid=8ae9d6&_nc_ohc=2muaceLlmEcAX-3PXVg&_nc_ht=scontent-dfw5-2.cdninstagram.com&oh=4e60563daef68d455b0af4ddfd64722d&oe=5F3E32FA"
+                alt=""></div>
+        <div class="item">
+            <img src="https://scontent-dfw5-1.cdninstagram.com/v/t51.29350-15/75281138_264807264618598_3672962236186155335_n.jpg?_nc_cat=105&_nc_sid=8ae9d6&_nc_ohc=iqq1sFHOeooAX9-MhlV&_nc_ht=scontent-dfw5-1.cdninstagram.com&oh=5a38d6bcfdcca4e4fa179a7149cc69ec&oe=5F3F7E86"
+                alt=""></div>
+
+        <div class="instagram">
+            <p>INSTAGRAM</p>
+        </div>
+
     </div>
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/f33c68b.png" alt="">
-        <div class="prod-list-title">SHAMPOO</div>
-        <div class="prod-list-sud-title">CLEANSING REPAIR & SMOOTH</div>
-        <div class="prod-list-intro"><br>エイトザタラソ <br> クレンジングリペア＆スムース<br> 美容液シャンプー<br>475mL/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
+    <div class="pre con">
+        <p>
+            ※1　クリスマムマリチマムカルス培養液（保湿）［エイトザタラソ モイストヘアオイル、エイトザタラソ スムースヘアオイル以外に配合］　※2　クリスマムマリチマムカルス培養液（保湿）［エイトザタラソ
+            モイストヘアオイル、エイトザタラソ スムースヘアオイル以外に配合］/アルガニアスピノサカルス培養エキス(保湿）［エイトザタラソ
+            モイストトリートメントに配合］／リンゴ果実培養細胞エキス(保湿）／ブドウ果実細胞エキス（保湿）［エイトザタラソ モイストシャンプー、エイトザタラソ
+            プレシャンプーに配合］／エリンギウムマリチムムカルス培養液（保湿）［エイトザタラソ スムースシャンプー、 エイトザタラソ スムーストリートメントに配合］</p>
     </div>
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/b41b477.png" alt="">
-        <div class="prod-list-title">TREATMENT</div>
-        <div class="prod-list-sud-title">SMOOTH REPAIR & AQUA SERUM</div>
-        <div class="prod-list-intro"><br>エイトザタラソ <br> スムースリペア＆アクアセラムト<br> 美容液トリートメント<br>475mL/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
+    <div class="footer">
+        <img src="http://eightthethalasso.jp/_nuxt/img/8251501.svg" alt="">
+        <div>
+            <span><i class="fab fa-instagram"></i></span>
+            <span><i class="fab fa-twitter"></i></span>
+        </div>
+        <div>
+            <a href="#">CONTACT US</a><a href="#">PRIVACY POLICY</a>
+        </div>
+        <div>ステラシード株式会社 〒107-0061 東京都港区北青山3-5-25表参道ビル4階 Tel.03-6804-5584 info@stellaseed.jp</div>
+        <div>©2019 8 THE THALASSO, All Rights Reserved.</div>
     </div>
-    <div class="item">
-        <img src="http://eightthethalasso.jp/_nuxt/img/184f733.png" alt="">
-        <div class="prod-list-title">HAIR MASK</div>
-        <div class="prod-list-sud-title">SMOOTH COAT & RICH SERUM</div>
-        <div class="prod-list-intro"><br>エイトザタラソ <br> スムースコート＆リッチセラム<br> 美容液ヘアマスク<br>200g/¥1400(税抜)</div>
-        <div><a href="#" class="prod-list-more block">More</a></div>
-    </div>
-    
-</div>
-   
+
 </body>
 
 </html>
