e9ade895fe01cd4171c4f603ed26f445629b4be1
kima-rin <anmoonlee@gmail.com>
2020-08-07 01:53:32 +0900

    5차모자시작

---
+++ public/PF/raemian/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>raemian</title>
+    <!-- 유용한 링크 -->
+    <!-- cdnsj : https://cdnjs.com/ -->
+    <!-- 폰트어썸 아이콘 리스트 : https://fontawesome.com/icons?d=gallery&m=free -->
+
+    <!-- 구글 폰트 불러오기 -->
+    <!-- rotobo(400/700/900), notosanskr(400/600/900) -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- 폰트어썸 불러오기 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+
+    <!-- 제이쿼리 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- 스와이퍼 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/js/swiper.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/css/swiper.min.css">
+    <script src="/PF/raemian/index.js"></script>
+    <link rel="stylesheet" href="/PF/raemian/index.css">
+</head>
+<body>
+    
+</body>
+</html>
