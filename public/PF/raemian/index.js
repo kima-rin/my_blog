1ec16185d5a1ef25032c30b36ea5207b5f502748
kima-rin <anmoonlee@gmail.com>
2020-08-07 18:41:15 +0900

    5차 모작 탑바

--- public/PF/raemian/index.js
+++ public/PF/raemian/index.js
@@ -1,1 +1,23 @@
 console.clear();
+
+function topBackground__init(){
+    var $topBar = $('.top-bar')
+  $topBar.find('.con > ul > li').mouseenter(function () {
+    $topBar.addClass('background-actived');
+  });
+
+  $topBar.mouseleave(function () {
+    $topBar.removeClass('background-actived');
+  });
+}
+function btnToggle__init(){
+    $('.btn-toggle-mobile-side-bar').click(function(){
+        $(this).toggleClass('active');
+    })
+}
+
+$(function(){
+    topBackground__init();
+    btnToggle__init();
+
+});
--- public/PF/raemian/index.php
+++ public/PF/raemian/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,7 +27,96 @@
     <script src="/PF/raemian/index.js"></script>
     <link rel="stylesheet" href="/PF/raemian/index.css">
 </head>
+
 <body>
-    
+<div class="top-bar flex con-min-width">
+     <div class="background"></div>
+     <div class="con flex">
+         <div class="flex-ai-c">
+             <div class="btn-toggle-mobile-side-bar flex-1-0-0">
+                 <div></div>
+                 <div></div>
+                 <div></div>
+             </div>
+         </div>
+         <ul class="flex flex-1-0-0">
+             <li><a href="#" class="block">단지정보</a>
+                 <ul>
+                     <li><a href="#">분양단지</a></li>
+                     <li><a href="#">공사단지</a></li>
+                     <li><a href="#">입주단지</a></li>
+                 </ul>
+             </li>
+             <li><a href="#" class="block">갤러리</a>
+                 <ul>
+                     <li><a href="#">갤러리소개</a></li>
+                     <li><a href="#">갤러리소식</a></li>
+                     <li><a href="#">래미안홈랩</a></li>
+                     <li><a href="#">래미안아카데미</a></li>
+                     <li><a href="#">래미안클라스</a></li>
+                     <li><a href="#">방문후기</a></li>
+                 </ul>
+             </li>
+             <li><a href="#" class="block">래미안타임즈</a>
+                 <ul>
+                     <li><a href="#">래미안투어</a></li>
+                     <li><a href="#">래미안라이프</a></li>
+                     <li><a href="#">부동산꿀팁</a></li>
+                     <li><a href="#">래미안뉴스</a></li>
+                     <li><a href="#">이벤트</a></li>
+                 </ul>
+             </li>
+             <li><a href="#" class="block">브랜드</a>
+                 <ul>
+                     <li><a href="#">브랜드소개</a></li>
+                     <li><a href="#">BI</a></li>
+                     <li><a href="#">브랜드히스토리</a></li>
+                     <li><a href="#">래미안상품</a></li>
+                     <li><a href="#">래미안광고</a></li>
+                 </ul>
+             </li>
+         </ul>
+         <div class="logo flex-1-0-0 flex-jc-c flex-ai-c"><img src="https://raemian.co.kr/assets/front/inc/images/layout/logo_h.png" alt=""></div>
+         <ul class="flex flex-1-0-0">
+             <li><a href="#" class="block">분양안내</a>
+                 <ul>
+                     <li><a href="#">분양캘린더</a></li>
+                     <li><a href="#">청약가이드</a></li>
+                     <li><a href="#">견본주택</a></li>
+                     <li><a href="#">전화상담예약</a></li>
+                     <li><a href="#">부동산세금</a></li>
+                     <li><a href="#">래미안시세</a></li>
+                 </ul>
+             </li>
+             <li><a href="#" class="block">계약·입주서비스</a>
+                 <ul>
+                     <li><a href="#">분양대금조회</a></li>
+                     <li><a href="#">입주안내</a></li>
+                     <li><a href="#">초청행사방문예약</a></li>
+                     <li><a href="#">입주예약</a></li>
+                     <li><a href="#">A/S</a></li>
+                     <li><a href="#">해스티아서비스</a></li>
+                     <li><a href="#">래미안 생활가이드</a></li>
+                     <li><a href="#">래미안웰스토리몰</a></li>
+                 </ul>
+             </li>
+             <li><a href="#" class="block">고객센터</a>
+                 <ul>
+                     <li><a href="#">고객센터안내</a></li>
+                     <li><a href="#">자주하는질문</a></li>
+                     <li><a href="#">1:1문의</a></li>
+                     <li><a href="#">A/S신청</a></li>
+                     <li><a href="#">사업소안내</a></li>
+                 </ul>
+             </li>
+         </ul>
+         <div class="icon-box-1 flex flex-1-0-0 flex-jc-end flex-ai-c">
+             <a href="#"><img src="https://raemian.co.kr/assets/front/inc/images/layout/btn_h_calendar.png" alt=""></a>
+             <a href="#"><img src="https://raemian.co.kr/assets/front/inc/images/layout/btn_h_mymenu.png" alt=""></a>
+         </div>
+     </div>
+ </div>
+
 </body>
+
 </html>
