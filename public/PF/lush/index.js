4c667755b95780b9618cfdbb447af1f4f7f1f1b7
kima-rin <anmoonlee@gmail.com>
2020-08-12 04:47:10 +0900

    lush top-bar

---
+++ public/PF/lush/index.js
@@ -0,0 +1,16 @@
+console.clear();
+
+function topBackground__init(){
+    var $topBar = $('.wrapper')
+  $topBar.find('.top-menu-box>.menu-box-base>.top-menu-bar>nav>ul>li').mouseenter(function () {
+    $topBar.addClass('background-actived');
+  });
+
+  $topBar.mouseleave(function () {
+    $topBar.removeClass('background-actived');
+  });
+}
+
+$(function(){
+    topBackground__init();
+});
