929d32979fb050d1978a0b340bc8ddf9a6a8d517
kima-rin <anmoonlee@gmail.com>
2020-08-30 21:28:37 +0900

    aboutMe

--- public/aboutMe.php
+++ public/aboutMe.php
@@ -1,7 +1,93 @@
 <?php
+include "../part/head_head.php";
+?>
+<?php
 include "../part/head.php";
 ?>
-<h1 class="con">About Me</h1>
+<div class="wrapper">
+    <div class="flex flex-ai-c flex-jc-c">
+        <div class="img-box flex flex-ai-c flex-jc-c">
+            <img src="https://kima-rin.github.io/img1/me/aboutMe.svg" alt="">
+        </div>
+        <div class="spec-box">
+            <div class="education">
+                <div class="title-box"><a class="title" href="#">education</a></div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date"><a href="#">2005.03-2010.02</a> </span>
+                    <span class="content">Bachelor of Science at Architectural Design Cource. <br>
+                        Seoul National University of Technology</span>
+                </div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date">2005.03-2010.02</span>
+                    <span class="content">Bachelor of Science at Architectural Design Cource. <br>
+                        Seoul National University of Technology</span>
+                </div>
+            </div>
+            <div class="award">
+                <div class="title-box"><a class="title" href="#">AWARD&EXHIBITION</a></div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date">2008.10.17 <br>~2008.10.11</span>
+                    <span class="content">Korea rural achitecture competion exigition , in COEX <br>
+                        Pacific Ocean hall</span>
+                </div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date">2008.10.17</span>
+                    <span class="content">Korea rural achitecture competion Silver Prize hosted by <br>
+                        Korea Rural corporation</span>
+                </div>
+            </div>
+            <div class="work">
+                <div class="title-box"><a class="title" href="#">WORK EXPERIENCE</a></div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date">2003.03-2004.02</span>
+                    <span class="content">make design drawing S-1 Corporation</span>
+                </div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date">2010.01-2011.02</span>
+                    <span class="content">Graphic designer KOREIT Co.,Ltd</span>
+                </div>
+                <div class="flex">
+                    <span class="deco"><i class="fas fa-caret-right"></i></span>
+                    <span class="date">2011.12-2019.12</span>
+                    <span class="content">Interior&Graphic designer G-Smatt Japan Co.,Ltd</span>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="skill-box">
+            <div class="softWare">
+                <div class="title-box-2"><a class="title" href="#">SOFT WARE</a></div>
+                <div><img src="https://kima-rin.github.io/img1/me/softwere.svg" width="250" alt=""></div>
+            </div>
+            <div class="skill">
+                <div class="title-box-2"><a class="title" href="#">skill</a></div>
+                <div><img src="https://kima-rin.github.io/img1/me/skill.svg" width="250" alt=""></div>
+            </div>
+            <div class="contect-box">
+                <div class="flex">
+                    <span><img src="https://kima-rin.github.io/img1/me/cellphone_1.svg" width="30" alt=""></span>
+                    <span class="flex-jc-c flex-ai-c"><a href="#">+82 10 5739 2141</a></span>
+                </div>
+                <div class="flex">
+                    <span><img src="https://kima-rin.github.io/img1/me/email_1.svg" width="30" alt=""></span>
+                    <span class="flex-jc-c flex-ai-c"><a href="#">anmoonlee@gmail.com</a></span>
+                </div>
+                <div class="flex">
+                    <span><img src="https://kima-rin.github.io/img1/me/spot_1.svg" width="30" alt=""></span>
+                    <span class="flex-jc-c flex-ai-c"><a href="#">	78, Sicheong-daero, Sejong-si</a></span>
+                </div>
+                <img src="https://kima-rin.github.io/img1/me/blog_qr.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 include "../part/foot.php";
 ?>
