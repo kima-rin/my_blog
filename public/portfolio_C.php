4a51f61b92902526f7fd4d373278340ae6a90d84
kima-rin <anmoonlee@gmail.com>
2020-09-03 00:27:38 +0900

    pf_c

--- public/portfolio_C.php
+++ public/portfolio_C.php
@@ -16,23 +16,93 @@ include "../part/portfolio_head.php";
         </div>
         <div class="content-list-box">
             <div class="flex">
-                <div class="con-1">
+                <div class="cont-1">
                     <h1 class="cont-number Poppins">01</h1>
-                    <p>AUG8 ---- 2020</p>
-                    <h2>IKEA</h2>
-                    <a href="https://www.ikea.com/kr/ko/">https://www.ikea.com/kr/ko/</a>
+                    <p class="cont-date">AUG8 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_01.svg" width="250" alt="">
+                        <p class="cont-url">https://www.ikea.com/kr/ko/</p>
+                    </div>
                     <div class="img-box">
-                        <p>project</p>
-                        <img src="https://kima-rin.github.io/img1/me/port_blank.png"alt="">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_01.png" alt="">
                     </div>
                 </div>
-                <div class="con-2">
-
+                <div class="cont-2">
+                    <h1 class="cont-number Poppins">02</h1>
+                    <p class="cont-date">AUG8 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_02.svg" width="250" alt="">
+                        <p class="cont-url">https://raemian.co.kr/main.do</p>
+                    </div>
+                    <div class="img-box">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_02.png" alt="">
+                    </div>
+                </div>
+                <div class="cont-3">
+                    <h1 class="cont-number Poppins">03</h1>
+                    <p class="cont-date">JUL7 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_03.svg" width="250" alt="">
+                        <p class="cont-url">https://www.lush.co.kr/</p>
+                    </div>
+                    <div class="img-box">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_03.png" alt="">
+                    </div>
+                </div>
+                <div class="cont-4">
+                    <h1 class="cont-number Poppins">04</h1>
+                    <p class="cont-date">JUL7 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_04.svg" width="250" alt="">
+                        <p class="cont-url">https://www.payletter.com/ko/</p>
+                    </div>
+                    <div class="img-box">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_04.png" alt="">
+                    </div>
+                </div>
+                <div class="cont-5">
+                    <h1 class="cont-number Poppins">05</h1>
+                    <p class="cont-date">JUN6 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_05.svg" width="250" alt="">
+                        <p class="cont-url">http://eightthethalasso.jp/</p>
+                    </div>
+                    <div class="img-box">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_05.png" alt="">
+                    </div>
+                </div>
+                <div class="cont-6">
+                    <h1 class="cont-number Poppins">06</h1>
+                    <p class="cont-date">JUN6 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_06.svg" width="250" alt="">
+                        <p class="cont-url">https://www.bonif.co.kr/</p>
+                    </div>
+                    <div class="img-box">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_06.png" alt="">
+                    </div>
+                </div>
+                <div class="cont-7">
+                    <h1 class="cont-number Poppins">07</h1>
+                    <p class="cont-date">JUN6 ---- 2020</p>
+                    <div class="cont-title-box">
+                        <img src="https://kima-rin.github.io/img1/me/co_t_07.svg" width="250" alt="">
+                        <p class="cont-url">https://www.kinfa.or.kr/index.do</p>
+                    </div>
+                    <div class="img-box">
+                        <p class="project">project</p>
+                        <img src="https://kima-rin.github.io/img1/me/port_07.png" alt="">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
 </section>
 
 <?php
