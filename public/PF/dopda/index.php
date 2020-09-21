23e82d0665e22a32d47bac166cadf0c81ab7e088
kima-rin <anmoonlee@gmail.com>
2020-09-21 14:15:44 +0900

    dopda

---
+++ public/PF/dopda/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dopda</title>
+    <!-- 구글 폰트 불러오기 -->
+    <!-- rotobo(400/700/900), notosanskr(400/600/900) -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <!-- 폰트어썸 불러오기 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+
+    <!-- 제이쿼리 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="/PF/dopda/index.js"></script>
+    <link rel="stylesheet" href="/PF/dopda/index.css">
+</head>
+
+<body>
+    <section class="top-menu-box">
+        <div class="flex border-red">
+            <div class="flex">
+                <img src="https://kima-rin.github.io/img1/pf/dopda/logo.png" height="35" alt="">
+            </div>
+            <ul class="flex flex-jc-e flex-grow-1">
+                <li><a href="#">About DOPDA</a></li>
+                <li><a href="#">SERVICES</a></li>
+                <li><a href="#">VIP MEMBERSHIP</a></li>
+                <li><a href="#">CONSULTING</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+            <div class="flex flex-jc-eborder-red">
+                <span><img src="https://kima-rin.github.io/img1/pf/dopda/search.png" alt=""></span>
+                <span><img src="https://kima-rin.github.io/img1/pf/dopda/mobile.png" alt=""></span>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
