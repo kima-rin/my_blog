1e848bb1f5f3ba3f45877b4b03e83d069f40ef32
kima-rin <anmoonlee@gmail.com>
2020-09-22 18:41:44 +0900

    dopda-slide-end

--- public/PF/dopda/index.js
+++ public/PF/dopda/index.js
@@ -1,28 +1,27 @@
 console.clear();
 
 function Slider1_1__init() {
-    var swiper = new Swiper('.slider-1-1 > .swiper-container', {
-      effect:'fade',
-      speed:0,
-      loop: true,
-      pagination: {
-        el: '.slider-1-1 .swiper-pagination',
-        type: 'fraction',
-      },
-      autoplay:{
-      delay:3000,
-     },
-    });
-  
-    var $count = $('.slider-1-1 .count');
-    swiper.on('slideChange', function () {
-      $count.text(swiper.realIndex + 1);
-    });
-  }
+  var swiper = new Swiper('.slider-1-1 > .swiper-container', {
+    effect: 'fade',
+    speed: 0,
+    loop: true,
+    pagination: {
+      el: '.slider-1-1 .swiper-pagination',
+      clickable: true
+    },
+    autoplay: {
+      delay: 3000,
+    },
+  });
 
-  
-  
-  $(function(){
+  var $count = $('.slider-1-1 .count');
+  swiper.on('slideChange', function () {
+    $count.text(swiper.realIndex + 1);
+  });
+}
+
+$(function () {
+  setTimeout(function() {
     Slider1_1__init();
-   })
-  
+  }, 500);
+})
--- public/PF/dopda/index.php
+++ public/PF/dopda/index.php
@@ -43,33 +43,14 @@
             </div>
         </div>
     </section>
-    <section class="relative">
+    <section class="slider-box relative">
         <div class="slider-1-1">
             <div class="swiper-container">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide img-box">
+                    <div class="swiper-slide">
                         <div class="slide-text">
                             <h1>For Your Business & Life</h1>
                             <p>돕다는 글로벌 컨시어지 서비스 그룹입니다. <br> 귀사와 귀하의 품격에 맞는 서비스를 제공합니다.</p>
-                            <ul class="bottom-text-box flex">
-                                <li><a href="#">
-                                        <strong>VIP Membership</strong>
-                                        <img src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt="">
-                                    </a>
-                                </li>
-                                <li><a href="#"><strong>오피스 컨시어지 서비스</strong><img
-                                            src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a>
-                                </li>
-                                <li><a href="#"><strong>라이프 컨시어지 서비스</strong><img
-                                            src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a>
-                                </li>
-                                <li><a href="#"><strong>정주지원 서비스</strong><img
-                                            src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a>
-                                </li>
-                                <li><a href="#"><strong>비즈니스 투어</strong><img
-                                            src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a>
-                                </li>
-                            </ul>
                         </div>
                         <div class="box-1">
                             <ul class="flex">
@@ -79,9 +60,11 @@
                                 <li><span></span></li>
                             </ul>
                         </div>
-                        <img src="https://kima-rin.github.io/img1/pf/dopda/main_visual01.jpg" alt="">
+                        <div class="img-box"><img src="https://kima-rin.github.io/img1/pf/dopda/main_visual01.jpg"
+                                alt=""></div>
+                        <div class="count-1"><img src="https://kima-rin.github.io/img1/pf/dopda/thum1.png" alt=""></div>
                     </div>
-                    <div class="swiper-slide img-box">
+                    <div class="swiper-slide">
                         <div class="slide-text">
                             <h1>For Your Business & Life</h1>
                             <p>돕다는 글로벌 컨시어지 서비스 그룹입니다. <br> 귀사와 귀하의 품격에 맞는 서비스를 제공합니다.</p>
@@ -94,9 +77,11 @@
                                 <li><span></span></li>
                             </ul>
                         </div>
-                        <img src="https://kima-rin.github.io/img1/pf/dopda/main_visual02.jpg" alt="">
+                        <div class="img-box"><img src="https://kima-rin.github.io/img1/pf/dopda/main_visual02.jpg"
+                                alt=""></div>
+                        <div class="count-1"><img src="https://kima-rin.github.io/img1/pf/dopda/thum2.png" alt=""></div>
                     </div>
-                    <div class="swiper-slide img-box">
+                    <div class="swiper-slide">
                         <div class="slide-text">
                             <h1>For Your Business & Life</h1>
                             <p>돕다는 글로벌 컨시어지 서비스 그룹입니다. <br> 귀사와 귀하의 품격에 맞는 서비스를 제공합니다.</p>
@@ -109,28 +94,41 @@
                                 <li><span></span></li>
                             </ul>
                         </div>
-                        <img src="https://kima-rin.github.io/img1/pf/dopda/main_visual03.jpg" alt="">
+                        <div class="img-box"><img src="https://kima-rin.github.io/img1/pf/dopda/main_visual03.jpg"
+                                alt="">
+                        </div>
+                        <div class="count-1"><img src="https://kima-rin.github.io/img1/pf/dopda/thum3.png" alt=""></div>
                     </div>
                 </div>
-                
-                <!-- Add Pagination -->
-                <div class="swiper-pagination"></div>
             </div>
-            <ul class="bottom-text-box flex border-red">
-                    <li><a href="#"><strong>VIP Membership</strong><img src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a>
-                    </li>
-                    <li><a href="#"><strong>오피스 컨시어지 서비스</strong><img
-                                src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
-                    <li><a href="#"><strong>라이프 컨시어지 서비스</strong><img
-                                src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
-                    <li><a href="#"><strong>정주지원 서비스</strong><img
-                                src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
-                    <li><a href="#"><strong>비즈니스 투어</strong><img
-                                src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination flex flex-column-nowrap"></div>
+            
+            <ul class="bottom-text-box flex">
+                <li class="flex flex-ai-c flex-1-0-0"><a href="#" class="flex flex-ai-c"><span>VIP Membership</span><img
+                        src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a>
+                </li>
+                <li class="flex flex-ai-c flex-1-0-0"><a href="#" class="flex flex-ai-c"><span>오피스 컨시어지 서비스</span><img
+                        src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
+                <li class="flex flex-ai-c flex-1-0-0"><a href="#" class="flex flex-ai-c"><span>라이프 컨시어지 서비스</span><img
+                        src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
+                <li class="flex flex-ai-c flex-1-0-0"><a href="#" class="flex flex-ai-c"><span>정주지원 서비스</span><img
+                        src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
+                <li class="flex flex-ai-c flex-1-0-0"><a href="#" class="flex flex-ai-c"><span>비즈니스 투어</span><img
+                        src="https://kima-rin.github.io/img1/pf/dopda/arrow_btn.png" alt=""></a></li>
             </ul>
-
         </div>
+    </section>
+    <section class="services-box">
+        <div>
+            <div class="img-box relative">
+                <img src="https://kima-rin.github.io/img1/pf/dopda/main2_bg.jpg" alt="">
+                <div class="services-text-box">
+                    <h1>글로벌 컨시어지 서비스, 돕다(DOPDA)와 함께 하세요.</h1>
 
+                </div>
+            </div>
+        </div>
     </section>
 </body>
 
